ddad4edfe78bf0fa69646e896eb0a3e00d36dfae
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-10-21 11:31:07 -0400

    added Canvas to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
+import Canvas from './Canvas.tsx';
 import './App.css';
 
 interface AppProps {}
@@ -15,25 +16,7 @@ function App({}: AppProps) {
   // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit !!!!<code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <Canvas />
     </div>
   );
 }
