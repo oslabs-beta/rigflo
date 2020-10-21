70539cb9e9b374e20e249e3898f69444d5467495
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-10-21 19:48:27 -0400

    added json pretty to format the json

--- src/components/Inspector.jsx
+++ src/components/Inspector.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/1337.css';
 
 const Inspector = ({ selectedEl }) => {
   return (
     <div id="inspector">
       <div>
-        <pre>{JSON.stringify(selectedEl)}</pre>
+        <JSONPretty
+          id="json-pretty"
+          data={selectedEl}
+          style={{ fontSize: '.64rem' }}
+          mainStyle="padding:1em"
+          valueStyle="font-size:1em"
+        ></JSONPretty>
       </div>
     </div>
   );
