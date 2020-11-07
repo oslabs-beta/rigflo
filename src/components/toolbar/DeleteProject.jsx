5f1c8c1bd6fde8d1682f3b6a064d2e496097d3e4
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-11-07 15:53:51 -0500

    added conditional text

--- src/components/toolbar/DeleteProject.jsx
+++ src/components/toolbar/DeleteProject.jsx
@@ -1,12 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { useElements } from '../../hooks/useElements';
 import { removeElements } from 'react-flow-renderer';
 
 const DeleteProject = () => {
   const [elements, setElements] = useElements();
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const handleDelete = () => {
     setElements(removeElements([elements], elements));
+    setIsDeleted(true);
   };
 
   return (
@@ -16,7 +18,7 @@ const DeleteProject = () => {
         onClick={handleDelete}
         className="inline-block px-4 py-2 mt-4 ml-4 text-sm leading-none text-white border-2 border-white rounded hover:border-transparent hover:text-purple-500 hover:bg-white lg:mt-0"
       >
-        Delete Project
+        {isDeleted ? 'Successfully Deleted' : 'Delete Project'}
       </a>
     </>
   );
