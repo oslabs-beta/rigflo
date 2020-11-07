1a18a9454f8125aad97914ba8becdac0734a3142
jamesscaggs <james.scaggs@gmail.com>
2020-11-07 11:54:43 -0600

    added callback route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './tailwind.output.css';
+import { Router, Link } from '@reach/router';
 
 import { ElementsProvider } from './hooks/useElements';
 import { ReactFlowProvider } from 'react-flow-renderer';
 import Layout from './Layout';
 import Sidebar from './components/Sidebar';
 import Canvas from './components/Canvas';
+import Callback from './pages/login/Callback';
 import { AuthProvider } from './context/AuthContext';
 
 const App = () => {
@@ -13,6 +15,9 @@ const App = () => {
     <ReactFlowProvider>
       <ElementsProvider>
         <AuthProvider>
+          <Router>
+            <Callback path="/login/callback" />
+          </Router>
           <Layout sidebar={<Sidebar />}>
             <Canvas />
           </Layout>
---
+++ src/pages/login/Callback.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Callback() {
+  console.log('Callback route');
+  return <h1>Calling back....</h1>;
+}
