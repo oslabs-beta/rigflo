7e95f22e049d43db768993fe159b05ac1f83cfd7
jamesscaggs <james.scaggs@gmail.com>
2020-11-07 14:05:04 -0600

    add cloudflare worker and configure callback url, store token in authstate. Still need to make a follow up request to get user data"

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import axios from 'axios';
 
 import { AuthContext } from '../context/AuthContext';
 
--- src/pages/login/Callback.jsx
+++ src/pages/login/Callback.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useLocation } from '@reach/router';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function Callback() {
-  // get the url query strings
+  const auth = useContext(AuthContext);
+  const { authState, setAuthState } = auth;
+
+  // Get auth code we receive back from github
   const location = useLocation();
   const urlParams = new URLSearchParams(location.search);
   const code = urlParams.get('code');
-  const state = urlParams.get('state');
+
+  authState.token = code;
+  authState.userInfo = {
+    name: 'James',
+    status: 'Still working on this feature :)',
+  };
+
+  // setAuthState(payload);
+
+  console.log(authState);
 
   console.log('Callback route');
-  return <h1>Calling back....</h1>;
+  return <h1>Auth Code: {code}.</h1>;
 }
