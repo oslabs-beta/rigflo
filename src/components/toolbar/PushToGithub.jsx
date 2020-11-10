31dd430c17f05edc680f2968def1a89f145c8438
jamesscaggs <james.scaggs@gmail.com>
2020-11-10 14:30:55 -0600

    starting express server

--- src/components/toolbar/PushToGithub.jsx
+++ src/components/toolbar/PushToGithub.jsx
@@ -1,28 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { useElements } from '../../hooks/useElements';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import axios from 'axios';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+export default function PushToGithub() {
+  const auth = useContext(AuthContext);
+  const { token } = auth.authState;
 
-const PushToGithub = () => {
+  async function fetchData() {
+    const config = {
+      method: 'POST',
+      url: `https://create-repo.rigflo.workers.dev/?token=${token}`,
+    };
+    const { data } = await axios(config);
+    console.log({ data });
+  }
+
   return (
     <>
-      <a
-        href="#"
+      <button
+        onClick={fetchData}
         className="inline-block px-4 py-2 mt-4 ml-4 text-sm leading-none text-white border-2 border-white rounded hover:border-transparent hover:text-purple-500 hover:bg-white lg:mt-0"
       >
         Push to Github
-      </a>
+      </button>
     </>
   );
-};
-
-export default PushToGithub;
+}
--- src/workers/oauth/create-repo.js
+++ src/workers/oauth/create-repo.js
@@ -2,9 +2,6 @@ addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
-const client_id = '9736e547efbf758aa0dc';
-const client_secret = '70f7de0cf4ef9031dbdb112632731be342384dd3';
-
 /**
  * Respond to the request
  * @param {Request} request
@@ -22,6 +19,10 @@ async function handleRequest(request) {
   }
 
   try {
+    // get the access token from the request url
+    const { searchParams } = new URL(request.url);
+    const token = searchParams.get('token');
+
     // redirect POST requests to the /user/repos
     if (request.method === 'POST') {
       const response = await fetch('https://api.github.com/user/repos', {
@@ -30,10 +31,10 @@ async function handleRequest(request) {
           Accept: 'application/vnd.github.v3+json',
           'Content-type': 'application/json',
           'User-Agent': 'request',
-          Authorization: 'token 798577036f555b1192566d45e51959e87c5898b2',
+          Authorization: `token ${token}`,
         },
         body: JSON.stringify({
-          name: 'Rigflo Test Repo',
+          name: `Rigflo YAML File ${Date.now()}`,
           description: 'Test number 2',
           homepage: 'https://example.com',
           private: false,
@@ -45,6 +46,8 @@ async function handleRequest(request) {
 
       console.log({ response });
 
+      const result = await response.json();
+
       const headers = {
         'Access-Control-Allow-Origin': '*',
       };
@@ -53,7 +56,7 @@ async function handleRequest(request) {
         return new Response(JSON.stringify(response), { status: 401, headers });
       }
 
-      return new Response(JSON.stringify({ message: 'Success!', response }), {
+      return new Response(JSON.stringify({ message: 'Success!', result }), {
         status: 201,
         headers,
       });
