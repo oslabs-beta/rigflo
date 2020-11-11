e14bd3c8e2df929c5f0a8d68d696961dc5df4e04
jamesscaggs <james.scaggs@gmail.com>
2020-11-11 05:36:22 -0600

    refactored save project to use less state

--- src/components/toolbar/SaveProject.jsx
+++ src/components/toolbar/SaveProject.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { useElements } from '../../hooks/useElements';
 import Modal from 'react-modal';
 
+// Modal styles
 const customStyles = {
   content: {
     top: '50%',
@@ -16,93 +16,60 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
+// helper
+const saveToDatabase = (uri, config = {}) => {
+  fetch(uri, config).then((res) => console.log(res));
+};
+
+// Saves a project to local storage and database
 const SaveProject = () => {
-  // Hook
-  const useLocalStorage = (key, initialValue) => {
-    // State to store our value
-    // Pass initial state function to useState so logic is only executed once
-    // console.log(key, initialValue);
-    const [storedValue, setStoredValue] = useState(() => {
-      try {
-        // Get from local storage by key
-        const item = window.localStorage.getItem(key);
-        // Parse stored json or if none return initialValue
-        return item ? JSON.parse(item) : initialValue;
-      } catch (error) {
-        // If error also return initialValue
-        console.log(error);
-        return initialValue;
-      }
-    });
-
-    // Return a wrapped version of useState's setter function that ...
-    // ... persists the new value to localStorage.
-    const setValue = (value) => {
-      try {
-        // Save state
-        setIsSaved(true);
-        setStoredValue(initialValue);
-        // Save to local storage
-        window.localStorage.setItem(key, JSON.stringify(initialValue));
-      } catch (error) {
-        // A more advanced implementation would handle the error case
-        console.log(error);
-      }
-    };
-    return [storedValue, setValue];
-  };
-
-  const [elements, setElements] = useElements();
-  const [isSaved, setIsSaved] = useState(false);
-  const [savedText, setSavedText] = useState('Save Project');
-  const [projectName, setProjectName] = useState('');
+  const [elements] = useElements();
   const [isModalOpen, setModalOpen] = useState(false);
-  const [project, setProject] = useLocalStorage(projectName, elements);
+
+  const initialProject = () => 'rigflo-project';
+  const [projectName, setProjectName] = useState(initialProject);
 
   const handleSave = (e) => {
     e.preventDefault();
-    setProject(projectName, elements);
+    console.log(e.target.value);
+
+    // update local storage
+    window.localStorage.setItem(projectName, JSON.stringify(elements));
+
+    // close modal
     setModalOpen(!isModalOpen);
+
+    // send to database
+    try {
+      console.log('sending to database');
+      saveToDatabase('http://localhost:3000/api/projects', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ project: elements }),
+      });
+    } catch (error) {
+      console.error({ error });
+      return error.message;
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setProjectName(value);
   };
 
   const toggleModal = () => {
     setModalOpen(!isModalOpen);
   };
 
-  function saveProject(uri, config = {}) {
-    fetch(uri, config).then((res) => console.log(res));
-  }
-
-  useEffect(() => {
-    console.log({ project });
-    saveProject('http://localhost:3000/api/projects', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ project: elements }),
-    });
-
-    console.log('sending to database');
-  }, [project]);
-
-  useEffect(() => {
-    setSavedText('Saved!');
-
-    const timer = setTimeout(() => {
-      setIsSaved(false);
-    }, 800);
-
-    return () => clearTimeout(timer);
-  }, [isSaved]);
-
   return (
     <>
-      <a
-        href="#"
+      <button
         onClick={toggleModal}
         className="inline-block px-4 py-2 mt-4 ml-4 text-sm leading-none text-white border-2 border-white rounded hover:border-transparent hover:text-purple-500 hover:bg-white lg:mt-0"
       >
-        {isSaved ? savedText : 'Save Project'}
-      </a>
+        Save Project
+      </button>
       <Modal
         isOpen={isModalOpen}
         onRequestClose={toggleModal}
@@ -110,7 +77,7 @@ const SaveProject = () => {
         contentLabel="Save project dialog"
       >
         <h1 className="text-center">Save Project</h1>
-        <form className="w-full max-w-sm">
+        <form className="w-full max-w-sm" onSubmit={handleSave}>
           <div className="flex items-center py-2 border-b border-teal-500">
             <input
               className="w-full px-2 py-1 mr-3 leading-tight text-gray-700 bg-transparent border-none appearance-none focus:outline-none"
@@ -118,12 +85,11 @@ const SaveProject = () => {
               placeholder="Project name"
               aria-label="Project name"
               value={projectName}
-              onChange={(e) => setProjectName(e.target.value)}
+              onChange={(e) => handleInputChange(e.target.value)}
             />
             <button
               className="flex-shrink-0 px-2 py-1 text-sm text-white bg-teal-500 border-4 border-teal-500 rounded hover:bg-teal-700 hover:border-teal-700"
-              type="button"
-              onClick={handleSave}
+              type="submit"
             >
               Save
             </button>
