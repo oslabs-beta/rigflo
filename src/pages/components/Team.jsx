e03c7aaaaf26c24286431f4efe83a90b9544a438
Akosua Kernizan <akosua@Akosuas-Air.home>
2020-11-12 15:49:06 -0500

    removing extra html, updating links

--- src/pages/components/Team.jsx
+++ src/pages/components/Team.jsx
@@ -9,41 +9,24 @@ const Team = (props) => {
             <h2 className='text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl'>
               Meet our team
             </h2>
-            <p className='text-xl leading-7 text-gray-500'>
-              Ornare sagittis, suspendisse in hendrerit quis. Sed dui aliquet
-              lectus sit pretium egestas vel mattis neque.
-            </p>
           </div>
           <ul className='mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-3 lg:max-w-5xl'>
             <li>
               <div className='space-y-6'>
                 <img
                   className='w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+                  src='https://ca.slack-edge.com/T016VMCKF7Y-U0185R5UJA1-34ec57058e50-512'
                   alt=''
                 />
                 <div className='space-y-2'>
                   <div className='space-y-1 text-lg font-medium leading-6'>
-                    <h4>Whitney Francis</h4>
-                    <p className='text-indigo-600'>Copywriter</p>
+                    <h4>Akosua Kernizan</h4>
+                    <p className='text-indigo-600'>Software Engineer</p>
                   </div>
                   <ul className='flex justify-center space-x-5'>
                     <li>
                       <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>Twitter</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/'
+                        href='https://www.linkedin.com/in/akosuakernizan/'
                         className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
                         <span className='sr-only'>LinkedIn</span>
                         <svg
@@ -58,6 +41,23 @@ const Team = (props) => {
                         </svg>
                       </a>
                     </li>
+                    {/* <li> */}
+                    {/* <a
+                        href='https://github.com/akernizan'
+                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
+                        <span className='sr-only'>Github</span>
+                        <svg
+                          className='w-5 h-5'
+                          fill='currentColor'
+                          viewBox='0 0 20 20'>
+                          <path
+                            fillRule='evenodd'
+                            d='M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z'
+                            clipRule='evenodd'
+                          />
+                        </svg>
+                      </a> */}
+                    {/* </li> */}
                   </ul>
                 </div>
               </div>
@@ -67,31 +67,18 @@ const Team = (props) => {
               <div className='space-y-6'>
                 <img
                   className='w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+                  src='https://ca.slack-edge.com/T016VMCKF7Y-U01A6FGPASV-9b33b8ce1b4f-512'
                   alt=''
                 />
                 <div className='space-y-2'>
                   <div className='space-y-1 text-lg font-medium leading-6'>
-                    <h4>Leonard Krasner</h4>
-                    <p className='text-indigo-600'>Senior Designer</p>
+                    <h4>Sam Carlile</h4>
+                    <p className='text-indigo-600'>Software Engineer</p>
                   </div>
                   <ul className='flex justify-center space-x-5'>
                     <li>
                       <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>Twitter</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/'
+                        href='https://www.linkedin.com/in/samkcarlile/'
                         className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
                         <span className='sr-only'>LinkedIn</span>
                         <svg
@@ -106,6 +93,23 @@ const Team = (props) => {
                         </svg>
                       </a>
                     </li>
+                    {/* <li> */}
+                    {/* <a
+                        href='https://github.com/samkcarlile'
+                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
+                        <span className='sr-only'>Github</span>
+                        <svg
+                          className='w-5 h-5'
+                          fill='currentColor'
+                          viewBox='0 0 20 20'>
+                          <path
+                            fillRule='evenodd'
+                            d='M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z'
+                            clipRule='evenodd'
+                          />
+                        </svg>
+                      </a> */}
+                    {/* </li> */}
                   </ul>
                 </div>
               </div>
@@ -115,31 +119,18 @@ const Team = (props) => {
               <div className='space-y-6'>
                 <img
                   className='w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+                  src='https://ca.slack-edge.com/T016VMCKF7Y-U019SHCMXLG-09f2fcc40a89-512'
                   alt=''
                 />
                 <div className='space-y-2'>
                   <div className='space-y-1 text-lg font-medium leading-6'>
-                    <h4>Floyd Miles</h4>
-                    <p className='text-indigo-600'>Principal Designer</p>
+                    <h4>Garrett James</h4>
+                    <p className='text-indigo-600'>Software Engineer</p>
                   </div>
                   <ul className='flex justify-center space-x-5'>
                     <li>
                       <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>Twitter</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/'
+                        href='https://www.linkedin.com/in/garrett-lamar-james/'
                         className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
                         <span className='sr-only'>LinkedIn</span>
                         <svg
@@ -154,6 +145,23 @@ const Team = (props) => {
                         </svg>
                       </a>
                     </li>
+                    {/* <li> */}
+                    {/* <a
+                        href='https://github.com/gjames5355'
+                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
+                        <span className='sr-only'>Github</span>
+                        <svg
+                          className='w-5 h-5'
+                          fill='currentColor'
+                          viewBox='0 0 20 20'>
+                          <path
+                            fillRule='evenodd'
+                            d='M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z'
+                            clipRule='evenodd'
+                          />
+                        </svg>
+                      </a> */}
+                    {/* </li> */}
                   </ul>
                 </div>
               </div>
@@ -163,31 +171,18 @@ const Team = (props) => {
               <div className='space-y-6'>
                 <img
                   className='w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
+                  src='../../assests/james.jpeg'
                   alt=''
                 />
                 <div className='space-y-2'>
                   <div className='space-y-1 text-lg font-medium leading-6'>
-                    <h4>Emily Selman</h4>
-                    <p className='text-indigo-600'>VP, User Experience</p>
+                    <h4>James Scaggs</h4>
+                    <p className='text-indigo-600'>Software Engineer</p>
                   </div>
                   <ul className='flex justify-center space-x-5'>
                     <li>
                       <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>Twitter</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/'
+                        href='https://www.linkedin.com/in/jamesscaggs/'
                         className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
                         <span className='sr-only'>LinkedIn</span>
                         <svg
@@ -202,42 +197,11 @@ const Team = (props) => {
                         </svg>
                       </a>
                     </li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-
-            <li>
-              <div className='space-y-6'>
-                <img
-                  className='w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
-                  alt=''
-                />
-                <div className='space-y-2'>
-                  <div className='space-y-1 text-lg font-medium leading-6'>
-                    <h4>Kristin Watson</h4>
-                    <p className='text-indigo-600'>VP, Human Resources</p>
-                  </div>
-                  <ul className='flex justify-center space-x-5'>
-                    <li>
-                      <a
-                        href='/'
+                    {/* <li> */}
+                    {/* <a
+                        href='https://github.com/akernizan'
                         className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>Twitter</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>LinkedIn</span>
+                        <span className='sr-only'>Github</span>
                         <svg
                           className='w-5 h-5'
                           fill='currentColor'
@@ -248,58 +212,8 @@ const Team = (props) => {
                             clipRule='evenodd'
                           />
                         </svg>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </li>
-
-            <li>
-              <div className='space-y-6'>
-                <img
-                  className='w-40 h-40 mx-auto rounded-full xl:w-56 xl:h-56'
-                  src='https://images.unsplash.com/photo-1505840717430-882ce147ef2d?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80'
-                  alt=''
-                />
-                <div className='space-y-2'>
-                  <div className='space-y-1 text-lg font-medium leading-6'>
-                    <h4>Emma Dorsey</h4>
-                    <p className='text-indigo-600'>
-                      Senior Front-end Developer
-                    </p>
-                  </div>
-                  <ul className='flex justify-center space-x-5'>
-                    <li>
-                      <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>Twitter</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path d='M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84' />
-                        </svg>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href='/'
-                        className='text-gray-400 transition duration-150 ease-in-out hover:text-gray-500'>
-                        <span className='sr-only'>LinkedIn</span>
-                        <svg
-                          className='w-5 h-5'
-                          fill='currentColor'
-                          viewBox='0 0 20 20'>
-                          <path
-                            fillRule='evenodd'
-                            d='M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z'
-                            clipRule='evenodd'
-                          />
-                        </svg>
-                      </a>
-                    </li>
+                      </a> */}
+                    {/* </li> */}
                   </ul>
                 </div>
               </div>
