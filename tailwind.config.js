36875f7f7ed16a4aa42e6a988eb3b3b3f0880c84
jamesscaggs <james.scaggs@gmail.com>
2020-11-13 12:30:03 -0600

    add git attributes and clean up packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['src/**/*.js'],
   theme: {
     extend: {
       fontFamily: {
