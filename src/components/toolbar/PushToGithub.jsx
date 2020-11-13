681a709e95d06be8323ebaf6b0ab62e1b31a549d
jamesscaggs <james.scaggs@gmail.com>
2020-11-13 12:50:55 -0600

    fix button styling

--- src/components/toolbar/PushToGithub.jsx
+++ src/components/toolbar/PushToGithub.jsx
@@ -25,7 +25,7 @@ export default function PushToGithub() {
     <>
       <button
         onClick={createRepo}
-        className="inline-block px-4 py-2 mt-4 ml-4 text-sm leading-none text-white border-2 border-white rounded hover:border-transparent hover:text-purple-500 hover:bg-white lg:mt-0"
+        className="inline-block px-4 py-2 mt-4 ml-4 text-sm font-bold leading-none text-white border-2 border-white rounded hover:border-transparent hover:text-purple-500 hover:bg-white lg:mt-0"
       >
         Push to Github
       </button>
